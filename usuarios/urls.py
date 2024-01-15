7428a3b6606e30499bcaefbee1f6c040dcaa2381
fpsnetodf <fps_netodf@hotmail.com>
2024-01-15 16:11:27 -0300

    flashcards

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
  path('cadastro/', views.cadastro, name='cadastro'),
- path('login/', views.logar, name='login'),
+ path('logar/', views.logar, name='login'),
+ path('logout/', views.logout, name='logout'),
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.contrib.auth.models import User
-from django.contrib import messages, auth
+from django.contrib.messages import add_message, constants
+from django.contrib import auth
 
 
 # Create your views here.
@@ -12,11 +14,13 @@ def cadastro(request):
         senha = request.POST.get('senha')
         confirmar_senha = request.POST.get('confirmar_senha')
     if not senha == confirmar_senha:
-        messages.add_message(request, messages.constants.ERROR, 'As senhas não coincídem')
+        # messages
+        add_message(request, constants.ERROR, 'As senhas não coincídem')
         return redirect('/usuarios/cadastro')
     user = User.objects.filter(username=username)
     if user.exists():
-        messages.add_message(request,messages.constants.ERROR,'Já existe um usuário com o mesmo username', )
+        # messages
+        add_message(request,constants.ERROR,'Já existe um usuário com o mesmo username', )
         return redirect('/usuarios/cadastro')
     try:
         user = User.objects.create_user(
@@ -24,9 +28,10 @@ def cadastro(request):
         password=confirmar_senha,
     )
 
-        return redirect('/usuarios/login')
+        return redirect(reverse('login'))
     except:
-        messages.add_message(request, messages.constants.ERROR, 'Erro interno do sistema',)
+        # messages
+        add_message(request, constants.ERROR, 'Erro interno do sistema',)
         return redirect('/usuarios/cadastro')
     
 def logar(request):
@@ -38,9 +43,16 @@ def logar(request):
         user = auth.authenticate(request, username=username, password=senha)
     if user:
         auth.login(request, user)
-        messages.add_message(request, messages.constants.SUCCESS, 'Logado!')
-        return redirect('/flashcard/novo_flashcard/')
+        # messages
+        add_message(request, constants.SUCCESS, 'Logado!')
+        return redirect(reverse('login'))
     else:
-        messages.add_message(request, messages.constants.ERROR, 'Username ou senha inválidos'
+         # messages
+        add_message(request, constants.ERROR, 'Username ou senha inválidos'
  )
-    return redirect('/usuarios/login')
+        return redirect('/usuarios/logar')
+    
+
+def logout(request):
+    auth.logout(request)    
+    return redirect('/usuarios/logar')
