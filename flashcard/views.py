b10a96a89066ee27199c12d6e14e52a935fa53a7
fpsnetodf <fps_netodf@hotmail.com>
2024-01-17 20:19:05 -0300

    agora tudo funciona

--- flashcard/views.py
+++ flashcard/views.py
@@ -1,5 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import Categoria
 # Create your views here.
 
 def novo_flashcard(request):
@@ -8,3 +9,12 @@ def novo_flashcard(request):
             request,
             'novo_flashcard.html',
         )
+    elif request.method == "POST":
+        cat = Categoria.objects.all()
+        
+        pergunta = request.POST.get("pergunta") 
+        resposta = request.POST.get("resposta")
+        categoria = request.POST.get("categoria")
+        dificuldade = request.POST.get("dificuldade")
+
+        return HttpResponse(f"{pergunta}, {resposta}, {categoria}, {dificuldade}")
